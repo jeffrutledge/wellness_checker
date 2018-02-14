b0df505a1d701c86bc477a8e33608f0b2bc9299a
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-02-14 11:13:32 -0800

    added install script for system daemon components

--- ask_about_happiness.py
+++ ask_about_happiness.py
@@ -4,6 +4,7 @@ import random
 import sys
 import csv
 import datetime
+import numpy as np
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -23,16 +24,18 @@ def rating_callback(notification, action):
 
 def closed_callback(notification):
     print('Closed notification.')
+    new_entry = [datetime.datetime.now().isoformat(), np.NaN]
+    with open('happiness_history.csv', 'a') as hist_csv:
+        writer = csv.writer(hist_csv)
+        writer.writerow(new_entry)
     Gtk.main_quit()
 
 
-if __name__ == "__main__":
-    if not notify2.init('Wellness Checker', mainloop='glib'):
-        sys.exit(1)
+def decide_to_ask(freq_to_ask, freq_run):
+    return random.random() < freq_to_ask / freq_run
 
-    if 'actions' not in notify2.get_server_caps():
-        sys.exit(1)
 
+def ask_about_happiness():
     happiness_check_notification = notify2.Notification(
         'Happiness Check', 'How happy are you right now?')
     happiness_check_notification.set_timeout(notify2.EXPIRES_NEVER)
@@ -48,3 +51,18 @@ if __name__ == "__main__":
         sys.exit(1)
 
     Gtk.main()
+
+
+if __name__ == "__main__":
+    if not notify2.init('Wellness Checker', mainloop='glib'):
+        sys.exit(1)
+
+    if 'actions' not in notify2.get_server_caps():
+        sys.exit(1)
+
+    if decide_to_ask(1 / 60, 1 / 10):
+        print('Asking about happiness.')
+        ask_about_happiness()
+    else:
+        print('Not asking about happiness.')
+    sys.exit(0)
